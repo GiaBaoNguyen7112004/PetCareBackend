025a564641cefd32559ac46e276fdaf62e73a5c1
BaoDev <giabao7112004@gmail.com>
2025-02-26 23:59:38 +0700

    implement request reset password, fix event listener

--- src/main/java/com/personalproject/universal_pet_care/event/listener/EmailListenerService.java
+++ src/main/java/com/personalproject/universal_pet_care/event/listener/EmailListenerService.java
@@ -1,77 +1,43 @@
 package com.personalproject.universal_pet_care.event.listener;
 
 import com.personalproject.universal_pet_care.entity.User;
-import com.personalproject.universal_pet_care.event.AppointmentApprovedEvent;
-import com.personalproject.universal_pet_care.event.AppointmentBookedEvent;
-import com.personalproject.universal_pet_care.event.AppointmentDeclinedEvent;
-import com.personalproject.universal_pet_care.event.UserRegistrationEvent;
-
-import com.personalproject.universal_pet_care.payload.request.VerificationTokenCreationRequest;
+import com.personalproject.universal_pet_care.event.*;
 import com.personalproject.universal_pet_care.service.email.EmailService;
 import com.personalproject.universal_pet_care.service.token.VerificationTokenService;
-import jakarta.mail.MessagingException;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
+
 import java.util.UUID;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class EmailNotificationListener {
+public class EmailListenerService {
     final EmailService emailService;
     final VerificationTokenService verificationTokenService;
 
     @Value("${frontend.base.url}")
     String frontendBaseUrl;
 
+    //    begin user registration event
     @EventListener
-    public void handleEvent(ApplicationEvent event) throws MessagingException, UnsupportedEncodingException {
-        log.info(event.getClass().getSimpleName());
-        switch (event.getClass().getSimpleName()) {
-            case "ServletWebServerInitializedEvent":
-                break;
-            case "UserRegistrationEvent":
-                handleUserRegistrationEvent((UserRegistrationEvent) event);
-                break;
-            case "AppointmentBookedEvent":
-                handleAppointmentBookingEvent((AppointmentBookedEvent) event);
-                break;
-            case "AppointmentApprovedEvent":
-                handleAppointmentApprovedEvent((AppointmentApprovedEvent) event);
-                break;
-            case "AppointmentDeclinedEvent":
-                handleAppointmentDeclinedEvent((AppointmentDeclinedEvent) event);
-                break;
-            default:
-        }
-    }
-
-    //  begin  handler user registration verification email
-    private void handleUserRegistrationEvent(UserRegistrationEvent event)
-            throws MessagingException, UnsupportedEncodingException {
+    private void handleUserRegistrationEvent(UserRegistrationEvent event) {
         User user = event.getUser();
         String verificationToken = UUID.randomUUID().toString();
 
-        verificationTokenService.saveVerificationTokenForUser(VerificationTokenCreationRequest.builder()
-                .userId(user.getId())
-                .token(verificationToken)
-                .build());
+        verificationTokenService.saveVerificationTokenForUser(user, verificationToken);
 
         String verificationUrl = frontendBaseUrl + "/email-verification?token=" + verificationToken;
         sendUserRegistrationVerificationEmail(user, verificationUrl);
     }
 
-    private void sendUserRegistrationVerificationEmail(User user, String url)
-            throws MessagingException, UnsupportedEncodingException {
+    private void sendUserRegistrationVerificationEmail(User user, String url) {
         String subject = "Email Verification - Universal Pet Care";
         String senderName = "Universal Pet Care Team";
         String mailContent = "<p>Dear " + user.getFirstName() + ",</p>" +
@@ -82,18 +48,17 @@ public class EmailNotificationListener {
                 "<p>Thank you,<br>The Universal Pet Care Team</p>";
         emailService.sendEmail(user.getEmail(), subject, senderName, mailContent);
     }
-//    end handle user registration verification email
+//    end user registration event
 
     //    begin handle appointment booked mail
-    private void handleAppointmentBookingEvent(AppointmentBookedEvent event)
-            throws MessagingException, UnsupportedEncodingException {
+    @EventListener
+    private void handleAppointmentBookingEvent(AppointmentBookedEvent event) {
         User veterinarian = event.getAppointment().getRecipient();
         String url = frontendBaseUrl;
         sendAppointmentBookingEmail(veterinarian, url);
     }
 
-    private void sendAppointmentBookingEmail(User veterinarian, String url)
-            throws MessagingException, UnsupportedEncodingException {
+    private void sendAppointmentBookingEmail(User veterinarian, String url) {
         String subject = "Notification of New Appointment";
         String senderName = "Universal Pet Care Team";
         String mailContent = "<p>Dear " + veterinarian.getFirstName() + ",</p>" +
@@ -109,15 +74,14 @@ public class EmailNotificationListener {
 //    end handle appointment booked mail
 
     //    begin handle appointment approved event
-    private void handleAppointmentApprovedEvent(AppointmentApprovedEvent event)
-            throws MessagingException, UnsupportedEncodingException {
+    @EventListener
+    private void handleAppointmentApprovedEvent(AppointmentApprovedEvent event) {
         User patient = event.getAppointment().getSender();
         String url = frontendBaseUrl;
         sendAppointmentApprovedEmail(patient, url);
     }
 
-    private void sendAppointmentApprovedEmail(User patient, String url)
-            throws MessagingException, UnsupportedEncodingException {
+    private void sendAppointmentApprovedEmail(User patient, String url) {
         String subject = "Appointment Approval Notification";
         String senderName = "Universal Pet Care Notification Service";
         String mailContent = "<p>Dear " + patient.getFirstName() + ",</p>" +
@@ -133,15 +97,14 @@ public class EmailNotificationListener {
 //    end handle appointment approved event
 
     //    begin handle appointment declined event
-    private void handleAppointmentDeclinedEvent(AppointmentDeclinedEvent event)
-            throws MessagingException, UnsupportedEncodingException {
+    @EventListener
+    private void handleAppointmentDeclinedEvent(AppointmentDeclinedEvent event) {
         User patient = event.getAppointment().getSender();
         String url = frontendBaseUrl;
         sendAppointmentDeclinedEmail(patient, url);
     }
 
-    private void sendAppointmentDeclinedEmail(User patient, String url)
-            throws MessagingException, UnsupportedEncodingException {
+    private void sendAppointmentDeclinedEmail(User patient, String url) {
         String subject = "Appointment Not Approved Notification";
         String senderName = "Universal Pet Care Notification Service";
         String mailContent = "<p>Dear " + patient.getFirstName() + ",</p>" +
@@ -161,4 +124,51 @@ public class EmailNotificationListener {
         emailService.sendEmail(patient.getEmail(), subject, senderName, mailContent);
     }
 //    end handle appointment declined event
+
+    //    begin handle appointment complete event
+    @EventListener
+    private void handleAppointmentCompleteEvent(AppointmentCompletedEvent event) {
+        User patient = event.getAppointment().getSender();
+        String url = frontendBaseUrl;
+        sendAppointmentCompletedEmail(patient, url);
+    }
+
+    private void sendAppointmentCompletedEmail(User patient, String url) {
+        String subject = "Appointment Completed Notification";
+        String senderName = "Universal Pet Care Notification Service";
+        String mailContent = "<p>Dear " + patient.getFirstName() + ",</p>" +
+                "<p>We are pleased to inform you that your appointment has been successfully completed." +
+                " Thank you for choosing our service.</p>" +
+                "<p>You can view more details about your appointment by clicking the link below:</p>" +
+                "<a href=\"" + url + "\">View Appointment Details</a><br/><br/>" +
+                "<p>If you have any further questions or need additional assistance, feel free to reach out to us.</p>" +
+                "<p>Best regards,</p>" +
+                "<p><strong>Universal Pet Care Notification Service</strong></p>";
+        emailService.sendEmail(patient.getEmail(), subject, senderName, mailContent);
+    }
+// end appointment complete event
+
+    //    begin password reset event
+    @EventListener
+    private void handlePasswordResetRequestEvent(PasswordResetEvent event) {
+        User user = event.getUser();
+        String token = UUID.randomUUID().toString();
+
+        verificationTokenService.saveVerificationTokenForUser(user, token);
+        String resetUrl = frontendBaseUrl + "/reset-password?token=" + token;
+        sendPasswordResetEmail(user, resetUrl);
+    }
+
+    private void sendPasswordResetEmail(User user, String resetUrl) {
+        String subject = "Password Reset Request - Universal Pet Care";
+        String senderName = "Universal Pet Care";
+        String mailContent = "<p>Dear " + user.getFirstName() + ",</p>" +
+                "<p>We received a request to reset your password. To proceed, please click the link below:</p>" +
+                "<p><a href=\"" + resetUrl + "\" style=\"color: #3498db; text-decoration: none; font-weight: bold;\">Reset Password</a></p>" +
+                "<p>If you did not request this change, you can safely ignore this email. Your account remains secure.</p>" +
+                "<p>Best regards,</p>" +
+                "<p><strong>Universal Pet Care Team</strong></p>";
+
+        emailService.sendEmail(user.getEmail(), subject, senderName, mailContent);
+    }
 }
